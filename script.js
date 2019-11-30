7c68b404ce0816e65fe2fc018e428f0d66eb3f4c
Dalekvim <dalekvim@gmail.com>
2019-11-30 13:59:36 +0000

    N/A

---
+++ script.js
@@ -0,0 +1,40 @@
+var chars = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ",", ".", "!", "?", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+function main() {
+
+    if (document.getElementById("shift").value == "") {
+        document.getElementById("shift").value = "1";
+    }
+
+    while (document.getElementById("shift").value > chars.length) {
+        document.getElementById("shift").value = document.getElementById("shift").value - chars.length;
+    }
+
+    shift = Number(document.getElementById("shift").value);
+
+    var message = document.getElementById("message").value;
+
+    var splitMessage = message.split("");
+
+    var encryptedMessage = [];
+
+    var shiftedIndex;
+
+    for (var i = 0; i < splitMessage.length; i++) {
+        shiftedIndex = chars.indexOf(splitMessage[i]) + shift;
+        console.log(shiftedIndex);
+        while (shiftedIndex >= chars.length) {
+            shiftedIndex = shiftedIndex - chars.length;
+        }
+        while (shiftedIndex < 0) {
+            shiftedIndex = shiftedIndex - chars.length;
+        }
+        encryptedMessage.push(chars[shiftedIndex]);
+    }
+    
+    document.getElementById("encryptedMessage").value = encryptedMessage.join("");
+
+}
+
+
+var loop = setInterval(main, 100);

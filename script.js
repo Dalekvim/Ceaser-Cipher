7b7876136d66bca8f168ed2dee7ea62027bb5115
Dalekvim <dalekvim@gmail.com>
2019-12-01 18:34:58 +0000

    N/A

--- script.js
+++ script.js
@@ -2,33 +2,37 @@ var chars = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 
 function main() {
 
-    if (document.getElementById("shift").value == "") {
-        document.getElementById("shift").value = "1";
-    }
 
-    while (document.getElementById("shift").value > chars.length) {
-        document.getElementById("shift").value = document.getElementById("shift").value - chars.length;
-    }
+    if (document.title != "Hacker") {
+        if (document.getElementById("shift").value == "") {
+            document.getElementById("shift").value = "1";
+        }
 
-    shift = Number(document.getElementById("shift").value);
+        while (document.getElementById("shift").value > chars.length) {
+            document.getElementById("shift").value = document.getElementById("shift").value - chars.length;
+        }
+
+        shift = Number(document.getElementById("shift").value);
+
+    }
 
     var message = document.getElementById("message").value;
 
     var splitMessage = message.split("");
 
-    if (document.title = "Encryptor") {
+    var shiftedIndex;
+
+    if (document.title == "Encryptor") {
         var encryptedMessage = [];
 
-        var shiftedIndex;
-
         for (var i = 0; i < splitMessage.length; i++) {
-            if (chars.indexOf(splitMessage[i]) !== "-1") {
+            if (chars.indexOf(splitMessage[i]) != "-1") {
                 shiftedIndex = chars.indexOf(splitMessage[i]) + shift;
                 while (shiftedIndex >= chars.length) {
                     shiftedIndex = shiftedIndex - chars.length;
                 }
                 while (shiftedIndex < 0) {
-                    shiftedIndex = shiftedIndex - chars.length;
+                    shiftedIndex = shiftedIndex + chars.length;
                 }
                 encryptedMessage.push(chars[shiftedIndex]);
             }
@@ -36,6 +40,51 @@ function main() {
     
         document.getElementById("encryptedMessage").value = encryptedMessage.join("");
 
+    } else if (document.title == "Decryptor") {
+        var decryptedMessage = [];
+
+        for (var i = 0; i < splitMessage.length; i++) {
+            if (chars.indexOf(splitMessage[i]) != "-1") {
+                shiftedIndex = chars.indexOf(splitMessage[i]) - shift;
+                while (shiftedIndex >= chars.length) {
+                    shiftedIndex = shiftedIndex - chars.length;
+                }
+                while (shiftedIndex < 0) {
+                    shiftedIndex = shiftedIndex + chars.length;
+                }
+                decryptedMessage.push(chars[shiftedIndex]);
+            }
+        }
+
+        document.getElementById("decryptedMessage").value = decryptedMessage.join("");
+    } else if (document.title == "Hacker") {
+
+        var decryptedMessages = [];
+        for (var shift = 0; shift < chars.length; shift++) {
+            var decryptedMessage = [];
+
+            for (var i = 0; i < splitMessage.length; i++) {
+                if (chars.indexOf(splitMessage[i]) != "-1") {
+                    shiftedIndex = chars.indexOf(splitMessage[i]) + shift;
+                    while (shiftedIndex >= chars.length) {
+                        shiftedIndex = shiftedIndex - chars.length;
+                    }
+                    while (shiftedIndex < 0) {
+                        shiftedIndex = shiftedIndex + chars.length;
+                    }
+                    decryptedMessage.push(chars[shiftedIndex]);
+                }
+            }
+
+            if (decryptedMessage != []) {
+                console.log(decryptedMessage.join(""));
+                if (decryptedMessage.join("").includes(document.getElementById("word").value)) {
+                   //document.getElementById("decryptedMessage").value = decryptedMessage.join("");
+                   decryptedMessages.push(decryptedMessage.join(""));
+                }
+            }
+        }
+        document.getElementById("decryptedMessage").value = decryptedMessages.join("\n");
     }
 
 }
